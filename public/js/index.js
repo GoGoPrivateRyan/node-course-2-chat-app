e2b22974a4588ec4d9e13748104a9fe2c51a318f
Ryan Tseng <gogoprivateryan@gmail.com>
2017-12-04 09:24:02 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'willie@example.com',
-    text: 'Count me one'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'ryan@example.com',
-    text: 'Have a lunch at 12:30.',
-    createdAt: 2345
-  });
-
   socket.on('createMessage', (message) => {
-    console.log('createMessage', ,essage);
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
